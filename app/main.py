b971e593cc471ab220dda1dea2d5fec9e2f78b91
urassh <shuto.1011@icloud.com>
2025-01-04 16:03:01 +0900

    fix: 環境変数が設定できていなかったのでその修正

--- app/main.py
+++ app/main.py
@@ -11,8 +11,8 @@ load_dotenv()
 POSTGRES_USER = os.getenv('POSTGRES_USER')
 POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 POSTGRES_DB = os.getenv('POSTGRES_DB')
-POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'db')  # デフォルト値
-POSTGRES_PORT = os.getenv('POSTGRES_PORT', 5432)  # デフォルト値
+POSTGRES_HOST = os.getenv('POSTGRES_HOST')
+POSTGRES_PORT = os.getenv('POSTGRES_PORT')
 
 app = Flask(__name__)
 
