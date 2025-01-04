431018fb29e5f5f9cf417af66e6695c8bae355c9
urassh <shuto.1011@icloud.com>
2025-01-04 13:40:01 +0900

    feat: v3の初期状態のための変更

--- app/main.py
+++ app/main.py
@@ -28,37 +28,7 @@ def create_tables():
 # メモ一覧を表示
 @app.route('/')
 def index():
-    memos = Memo.query.order_by(Memo.created_at.desc()).all()
-    return render_template('index.html', memos=memos)
-
-# 新しいメモの作成フォームを表示
-@app.route('/create', methods=['GET'])
-def show_create_memo():
-    return render_template('create_memo.html')
-
-# 新しいメモを作成
-@app.route('/create', methods=['POST'])
-def create_memo():
-    title = request.form['title']
-    content = request.form['content']
-    new_memo = Memo(title=title, content=content)
-    db.session.add(new_memo)
-    db.session.commit()
-    return redirect(url_for('index'))
-
-# メモの詳細を表示
-@app.route('/memo/<int:memo_id>')
-def view_memo(memo_id):
-    memo = Memo.query.get_or_404(memo_id)
-    return render_template('view_memo.html', memo=memo)
-
-# メモを削除
-@app.route('/memo/<int:memo_id>/delete', methods=['POST'])
-def delete_memo(memo_id):
-    memo = Memo.query.get_or_404(memo_id)
-    db.session.delete(memo)
-    db.session.commit()
-    return redirect(url_for('index'))
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
