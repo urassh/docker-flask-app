5ca070c8ae2c0ba9df099cf30fe1304dc36a514b
urassh <shuto.1011@icloud.com>
2025-01-04 16:04:44 +0900

    fix: conflict

--- app/main.py
+++ app/main.py
@@ -1,22 +1,4 @@
-<<<<<<< HEAD
-from flask import Flask, render_template, request, redirect, url_for
-from app.models import db, Memo
-from dotenv import load_dotenv
-import os
-from uuid import UUID
-
-# .envファイルを読み込む
-load_dotenv()
-
-# 環境変数からデータベース接続情報を取得
-POSTGRES_USER = os.getenv('POSTGRES_USER')
-POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
-POSTGRES_DB = os.getenv('POSTGRES_DB')
-POSTGRES_HOST = os.getenv('POSTGRES_HOST')
-POSTGRES_PORT = os.getenv('POSTGRES_PORT')
-=======
 from flask import Flask
->>>>>>> 64dd4af (fix: step1用に修正)
 
 app = Flask(__name__)
 
