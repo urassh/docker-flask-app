7c7f2fd913a30cba40c05074c9d4c916bd7e8705
urassh <shuto.1011@icloud.com>
2025-01-04 16:48:28 +0900

    fix: STEP4用にプロジェクトを修正

--- app/main.py
+++ app/main.py
@@ -32,34 +32,28 @@ def index():
     memos = Memo.query.order_by(Memo.created_at.desc()).all()
     return render_template('index.html', memos=memos)
 
-# 新しいメモの作成フォームを表示
-@app.route('/create', methods=['GET'])
-def show_create_memo():
-    return render_template('create_memo.html')
-
-# 新しいメモを作成
-@app.route('/create', methods=['POST'])
-def create_memo():
-    title = request.form['title']
-    content = request.form['content']
-    new_memo = Memo(title=title, content=content)
-    db.session.add(new_memo)
-    db.session.commit()
-    return redirect(url_for('index'))
-
 # メモの詳細を表示
 @app.route('/memo/<uuid:memo_id>')
 def view_memo(memo_id):
     memo = Memo.query.get_or_404(str(memo_id))
     return render_template('view_memo.html', memo=memo)
 
-# メモを削除
-@app.route('/memo/<uuid:memo_id>/delete', methods=['POST'])
-def delete_memo(memo_id):
-    memo = Memo.query.get_or_404(str(memo_id))
-    db.session.delete(memo)
-    db.session.commit()
-    return redirect(url_for('index'))
+# 新しいメモの作成フォームを表示
+# ===============================
+# エンドポイント /create
+# メソッド　　GET
+# 返すもの　create_memo.html
+# ===============================
+
+
+# 新しいメモを作成
+# ===============================
+# エンドポイント /create
+# メソッド　　POST
+# 返すもの　index.html (リダイレクト)
+# ===============================
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
